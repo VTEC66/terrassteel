665acaa79224098d2b496e23b7255ac9a755f2fc
julien <j.paput@gmail.com>
2019-11-19 18:12:53 +0100

    Dev on AddConstructionScreen

--- app/src/main/java/com/vtec/terrassteel/home/construction/ui/AddConstructionActivity.java
+++ app/src/main/java/com/vtec/terrassteel/home/construction/ui/AddConstructionActivity.java
@@ -3,11 +3,17 @@ package com.vtec.terrassteel.home.construction.ui;
 import android.os.Bundle;
 
 import com.vtec.terrassteel.R;
+import com.vtec.terrassteel.common.listener.ActionBarListener;
+import com.vtec.terrassteel.common.ui.ActionBar;
 import com.vtec.terrassteel.core.ui.AbstractActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddConstructionActivity extends AbstractActivity {
+
+    @BindView(R.id.action_bar)
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,17 @@ public class AddConstructionActivity extends AbstractActivity {
         ButterKnife.bind(this);
 
 
+        actionBar.setListener(new ActionBarListener() {
+            @Override
+            public void onBackArrowClick() {
+                finish();
+            }
+
+            @Override
+            public void onActionButtonClick() {
+
+            }
+        });
     }
 
 }
