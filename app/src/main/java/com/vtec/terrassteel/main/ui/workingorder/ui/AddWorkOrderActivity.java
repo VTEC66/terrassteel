5381cf328506e682b1d29d68447b7bb4c51e12cb
julien <j.paput@gmail.com>
2019-11-26 16:44:58 +0100

    Add WorkOrder screen UI

---
+++ app/src/main/java/com/vtec/terrassteel/main/ui/workingorder/ui/AddWorkOrderActivity.java
@@ -0,0 +1,139 @@
+package com.vtec.terrassteel.main.ui.workingorder.ui;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+
+import com.vtec.terrassteel.R;
+import com.vtec.terrassteel.core.ui.AbstractActivity;
+
+import androidx.annotation.Nullable;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.vtec.terrassteel.core.Const.NO_ERROR_CODE;
+import static com.vtec.terrassteel.core.Const.VIBRATION_DURATION;
+
+public class AddWorkOrderActivity extends AbstractActivity {
+
+
+    private static final int ERROR_EMPTY_WORK_ORDER_REFERENCE_FIELD = 2;
+    private static final int ERROR_EMPTY_AFFAIRE_REFERENCE_FIELD = 3;
+    private static final int ERROR_EMPTY_PRODUCT_TYPE_FIELD = 4;
+    private static final int ERROR_EMPTY_WORK_ORDER_HOUR_FIELD = 5;
+
+    @OnClick(R.id.validate_button)
+    public void onClickValidateButton(){
+
+        clearHighlightErrors();
+
+        int error = controlField();
+
+        if (error == NO_ERROR_CODE) {
+            addNewWorkOrder();
+        } else {
+
+            Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
+
+            if (vibrator != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(VIBRATION_DURATION, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    vibrator.vibrate(VIBRATION_DURATION);
+                }
+            }
+            highlightError(error);
+        }
+    }
+    @BindView(R.id.workorder_reference_textinputlayout)
+    View workOrderReferenceTil;
+
+    @BindView(R.id.workorder_reference_edittext)
+    EditText workOrderReferenceEdittext;
+
+    @BindView(R.id.workorder_affaire_textinputlayout)
+    View workOrderAffaireTil;
+
+    @BindView(R.id.workorder_affaire_edittext)
+    EditText workOrderAffaireEdittext;
+
+    @BindView(R.id.workorder_producttype_textinputlayout)
+    View workOrderProductTypeTil;
+
+    @BindView(R.id.workorder_producttype_edittext)
+    EditText workOrderProductTypeEdittext;
+
+    @BindView(R.id.workorder_hours_textinputlayout)
+    View workOrderHoursTil;
+
+    @BindView(R.id.workorder_hours_edittext)
+    EditText workOrderHoursEdittext;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.add_workorder_activity);
+
+        ButterKnife.bind(this);
+    }
+
+    private int controlField() {
+        if (workOrderReferenceEdittext.getText().length() == 0) {
+            return ERROR_EMPTY_WORK_ORDER_REFERENCE_FIELD;
+        }
+
+        if (workOrderAffaireEdittext.getText().length() == 0) {
+            return ERROR_EMPTY_AFFAIRE_REFERENCE_FIELD;
+        }
+
+        if (workOrderProductTypeEdittext.getText().length() == 0) {
+            return ERROR_EMPTY_PRODUCT_TYPE_FIELD;
+        }
+
+        if (workOrderHoursEdittext.getText().length() == 0) {
+            return ERROR_EMPTY_WORK_ORDER_HOUR_FIELD;
+        }
+
+        return NO_ERROR_CODE;
+    }
+
+    private void highlightError(int error) {
+        switch (error) {
+            case ERROR_EMPTY_WORK_ORDER_REFERENCE_FIELD:
+                workOrderReferenceTil.startAnimation(AnimationUtils.loadAnimation(this,R.anim.shake));
+                workOrderReferenceEdittext.setError(getString(R.string.standard_mandatory_field_message));
+                break;
+            case ERROR_EMPTY_AFFAIRE_REFERENCE_FIELD:
+                workOrderAffaireTil.startAnimation(AnimationUtils.loadAnimation(this,R.anim.shake));
+                workOrderAffaireEdittext.setError(getString(R.string.customer_mandatory_field_message));
+                break;
+            case ERROR_EMPTY_PRODUCT_TYPE_FIELD:
+                workOrderProductTypeTil.startAnimation(AnimationUtils.loadAnimation(this,R.anim.shake));
+                workOrderProductTypeEdittext.setError(getString(R.string.standard_mandatory_field_message));
+                break;
+            case ERROR_EMPTY_WORK_ORDER_HOUR_FIELD:
+                workOrderHoursTil.startAnimation(AnimationUtils.loadAnimation(this,R.anim.shake));
+                workOrderHoursEdittext.setError(getString(R.string.customer_mandatory_field_message));
+                break;
+        }
+    }
+
+    private void addNewWorkOrder() {
+
+    }
+
+    private void clearHighlightErrors() {
+        workOrderReferenceEdittext.setError(null);
+        workOrderAffaireEdittext.setError(null);
+        workOrderProductTypeEdittext.setError(null);
+        workOrderHoursEdittext.setError(null);
+    }
+}
--- app/src/main/java/com/vtec/terrassteel/main/ui/workingorder/ui/WorkingOrderFragment.java
+++ app/src/main/java/com/vtec/terrassteel/main/ui/workingorder/ui/WorkingOrderFragment.java
@@ -1,5 +1,6 @@
 package com.vtec.terrassteel.main.ui.workingorder.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,15 +8,26 @@ import android.view.ViewGroup;
 
 import com.vtec.terrassteel.R;
 import com.vtec.terrassteel.core.ui.AbstractFragment;
+import com.vtec.terrassteel.home.company.customer.ui.EditCustomerActivity;
 import com.vtec.terrassteel.main.ui.dashboard.ui.DashboardFragment;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WorkingOrderFragment extends AbstractFragment {
 
+    private static  final int ADD_CUSTOMER_INTENT_CODE = 19;
+
     public static final String TAG = WorkingOrderFragment.class.getSimpleName();
+
+    @OnClick(R.id.add_workorder_button)
+    public void onClickAddWorkOrder(){
+        startActivityForResult(new Intent(getContext(), AddWorkOrderActivity.class), ADD_CUSTOMER_INTENT_CODE);
+        getActivity().overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
 
     @Nullable
     @Override
